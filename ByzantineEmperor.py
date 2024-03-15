4c3f8946c3234b1fd87fcf96bfd78bc2c6a1414d
Michael Stamatis <82336443+mixstam1821@users.noreply.github.com>
2024-03-16 01:11:59 +0200

    Update ByzantineEmperor.py

--- ByzantineEmperor.py
+++ ByzantineEmperor.py
@@ -1,8 +1,9 @@
 class ByzantineEmperor:
-    def __init__(self, name, reign_start, reign_end):
+    def __init__(self, name, reign_start, reign_end, notable_achievements):
         self.name = name
         self.reign_start = reign_start
         self.reign_end = reign_end
+        self.notable_achievements = notable_achievements
 
     def length_of_reign(self):
         return self.reign_end - self.reign_start
@@ -26,10 +27,10 @@ class ByzantineEmpire:
 # Example usage:
 byzantine_empire = ByzantineEmpire()
 
-emperor1 = ByzantineEmperor("Justinian I", 527, 565)
-emperor2 = ByzantineEmperor("Constantine XI", 1449, 1453)
-emperor3 = ByzantineEmperor("Alexios I Komnenos", 1081, 1118)
-emperor4 = ByzantineEmperor("Basil II", 976, 1025)
+emperor1 = ByzantineEmperor("Justinian I", 527, 565, "Reconquest of the Western Roman Empire, Corpus Juris Civilis")
+emperor2 = ByzantineEmperor("Constantine XI", 1449, 1453, "Last Byzantine Emperor, Fall of Constantinople")
+emperor3 = ByzantineEmperor("Alexios I Komnenos", 1081, 1118, "First Crusade, Komnenian Restoration")
+emperor4 = ByzantineEmperor("Basil II", 976, 1025, "Bulgarian Wars, Byzantine Renaissance")
 
 byzantine_empire.add_emperor(emperor1)
 byzantine_empire.add_emperor(emperor2)
@@ -45,5 +46,6 @@ selected_emperor = byzantine_empire.select_emperor(selected_emperor_name)
 if selected_emperor:
     print("Selected Emperor:", selected_emperor)
     print("Length of reign:", selected_emperor.length_of_reign(), "years")
+    print("Notable Achievements:", selected_emperor.notable_achievements)
 else:
     print("Emperor not found")
